5fea60de5171c89b771628a7f8e4e7b74ae5b65c
bfssxv <bfssxv@neptun.gde.hu>
2024-06-01 20:23:09 +0200

    add Race repository and Entity

--- runners/src/main/java/com/example/runners/model/RaceEntity.java
+++ runners/src/main/java/com/example/runners/model/RaceEntity.java
@@ -5,14 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 @Entity
-public class RunnerEntity {
+public class RaceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long runnerId;
-    private String runnerName;
-    private int runnerAge;
-    private enum runnerGender {male,female};
+    public long raceId;
+    private String raceName;
 
+    private float raceDistance;
 }
---
+++ runners/src/main/java/com/example/runners/repository/RaceRepository.java
@@ -0,0 +1,12 @@
+package com.example.runners.repository;
+
+import com.example.runners.model.RaceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceRepository extends JpaRepository<RaceEntity,Long > {
+}
+
+
+
+
+
